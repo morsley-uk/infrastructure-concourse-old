1b6d0597916d4750896da58ce047c7a4935020be
John Morsley <johnmorsley@gmail.com>
2020-02-17 22:15:06 +0000

    Completed AMI, added EC2

--- aws/main.tf
+++ aws/main.tf
@@ -1,3 +1,9 @@
+###############################################################################
+# VPC - Virtual Private Cloud
+###############################################################################
+
+# https://registry.terraform.io/modules/terraform-aws-modules/vpc/aws/2.24.0
+
 module "vpc" {
   
   source  = "terraform-aws-modules/vpc/aws"
@@ -8,7 +14,7 @@ module "vpc" {
 
   azs             = ["eu-west-2a"]
   private_subnets = ["10.0.1.0/24"]
-  public_subnets  = ["10.0.101.0/24"]
+  public_subnets  = ["10.0.2.0/24"]
 
   enable_nat_gateway = true
   enable_vpn_gateway = true
@@ -21,40 +27,76 @@ module "vpc" {
   
 }
 
+###############################################################################
+# AMI - Amazon Machine Image
+###############################################################################
+
+# https://cloud-images.ubuntu.com/locator/ec2/
+
 data "aws_ami" "ubuntu" {
   
   most_recent = true
-  owners = ["amazon"]
+  owners = ["099720109477"] # Canonical
   
   filter {
-    name = "platform"
-    values = ["ubuntu"]
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
   }
 
-  #Root device type: ebs 
-  #Virtualization type: hvm 
-  #ENA Enabled: Yes
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
   
 }
-//module "ec2-instance" {
-//
-//  source  = "terraform-aws-modules/ec2-instance/aws"
-//  version = "2.12.0"
-//
-//  name                   = "concourse"
-//  instance_count         = 1
-//
-//  ami                    = "ami-0fc20dd1da406780b"
-//  instance_type          = "t2.micro"
-//  key_name               = "user1"
-//  monitoring             = true
-//  vpc_security_group_ids = ["sg-12345678"]
-//  subnet_id              = "subnet-eddcdzz4"
-//
-//  tags = {
-//    Terraform   = "true"
-//    Environment = "Development"
-//    Operation   = "Concourse"
-//  }
-//  
-//}
+
+###############################################################################
+# Key Pair
+###############################################################################
+
+# https://registry.terraform.io/modules/terraform-aws-modules/key-pair/aws/0.2.0
+
+module "key-pair" {
+  
+  source  = "terraform-aws-modules/key-pair/aws"
+  version = "0.2.0"
+  
+  key_name = "morsley-uk-concourse"
+  public_key = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDEqt9Wta8swnodwBJA5Hg3Ve3yXWATie4lPXklMC/Px2aEMNcnHP0rbJARAGYXKMxflQ+txoUtFbx7Ub4b4N2fbPsEc6oyMmyNJNy5cSk1q6hoyS9rB1AvCZjZEOvLC4BZFNimwQpNxOhxsC4rJhzrMW2+C3aETEBDQkT5UEPvOQIDAQAB"
+  
+}
+
+###############################################################################
+# EC2 - Elastic Compute Cloud
+###############################################################################
+
+# https://registry.terraform.io/modules/terraform-aws-modules/ec2-instance/aws/2.12.0
+
+module "ec2-instance" {
+
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "2.12.0"
+
+  name                   = "concourse"
+  instance_count         = 1
+
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t2.micro"
+  key_name               = "morsley-uk-concourse"
+  monitoring             = true
+  vpc_security_group_ids = [module.vpc.default_security_group_id]
+  subnet_id              = module.vpc.private_subnets[0]
+
+  tags = {
+    Terraform   = "true"
+    Environment = "Development"
+    Operation   = "Concourse"
+  }
+  
+}
+
+###############################################################################
+# Route 53
+###############################################################################
+
+# concourse.morsley.io
