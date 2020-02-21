5a74e424fec08a3e3cc8a3eaf7aa6e37af7aabb8
John Morsley <johnmorsley@gmail.com>
2020-02-21 19:20:26 +0000

    DNS?

--- aws/output.tf
+++ aws/output.tf
@@ -1,35 +1,35 @@
-# output "vpc_id" {
+output "vpc_id" {
   
-#   value = module.vpc.vpc_id
+  value = module.vpc.vpc_id
   
-# }
+}
 
-# output "public_subnets" {
+output "public_subnets" {
 
-#   value = module.vpc.public_subnets
+  value = module.vpc.public_subnets
 
-# }
+}
 
-# output "private_subnets" {
+output "private_subnets" {
 
-#   value = module.vpc.private_subnets
+  value = module.vpc.private_subnets
 
-# }
+}
 
-# output "security_group_id" {
+output "security_group_id" {
   
-#   value = module.vpc.default_security_group_id
+  value = module.vpc.default_security_group_id
   
-# }
+}
 
-# output "key_fingerprint" {
+output "key_fingerprint" {
   
-#   value = module.key-pair.this_key_pair_fingerprint
+  value = module.key-pair.this_key_pair_fingerprint
   
-# }
+}
 
-# output "ami_id" {
+output "ami_id" {
   
-#   value = data.aws_ami.ubuntu.id
+  value = data.aws_ami.ubuntu.id
   
-# }
+}
