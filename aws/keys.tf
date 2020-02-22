8f0f2fb392cbf573ff4d8133c97d6436b07af73c
John Morsley <johnmorsley@gmail.com>
2020-02-22 23:36:08 +0000

    Tidy

--- aws/keys.tf
+++ aws/keys.tf
@@ -17,55 +17,11 @@ resource "aws_key_pair" "key_pair" {
 
 #
 
-resource "aws_s3_bucket" "public_keys" {
-
-  bucket = var.public_keys_bucket
-  acl    = "private"
-
-  versioning {
-    enabled = true
-  }
-
-  lifecycle {
-    prevent_destroy = false
-  }
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-}
-
 resource "aws_s3_bucket_object" "public_keys" {
   bucket     = var.public_keys_bucket
   key        = "/${var.key_name}.pub"
   content    = join("", tls_private_key.private_key.*.public_key_openssh)
   depends_on = [aws_s3_bucket.public_keys]
-}
-
-resource "aws_s3_bucket" "private_keys" {
-
-  bucket = var.private_keys_bucket
-  acl    = "private"
-
-  versioning {
-    enabled = true
-  }
-
-  lifecycle {
-    prevent_destroy = false
-  }
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
 }
 
 resource "aws_s3_bucket_object" "private_keys" {
--- aws/s3.tf
+++ aws/s3.tf
@@ -2,8 +2,48 @@
 # S3
 ###############################################################################
 
-//resource "aws_s3_bucket" "bucket" {
-//  
-//  bucket = var.keys_bucket_name
-//  
-//}
+#
+
+resource "aws_s3_bucket" "public_keys" {
+
+  bucket = var.public_keys_bucket
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle {
+    prevent_destroy = false
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_s3_bucket" "private_keys" {
+
+  bucket = var.private_keys_bucket
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle {
+    prevent_destroy = false
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
